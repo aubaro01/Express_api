51800780078545a469fa0c2e62f187e806c2925a
Cláudio Barroso <claudiobarroso106@gmail.com>
2025-04-11 22:55:59 +0100

    up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,15 @@
-const Usuario = require('../models/Usuario');
+const user = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.registrar = async (req, res) => {
   const { nome, email, senha } = req.body;
 
-  const usuarioExistente = await Usuario.findOne({ email });
+  const usuarioExistente = await user.findOne({ email });
   if (usuarioExistente) return res.status(400).json({ msg: 'Usuário já existe' });
 
   const hashedSenha = await bcrypt.hash(senha, 10);
-  const novoUsuario = await Usuario.create({ nome, email, senha: hashedSenha });
+  const novoUsuario = await user.create({ nome, email, senha: hashedSenha });
 
   res.status(201).json({ msg: 'Usuário criado com sucesso!' });
 };
@@ -17,13 +17,13 @@ exports.registrar = async (req, res) => {
 exports.login = async (req, res) => {
   const { email, senha } = req.body;
 
-  const usuario = await Usuario.findOne({ email });
-  if (!usuario) return res.status(404).json({ msg: 'Usuário não encontrado' });
+  const user = await user.findOne({ email });
+  if (!user) return res.status(404).json({ msg: 'Usuário não encontrado' });
 
-  const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
+  const senhaCorreta = await bcrypt.compare(senha, user.senha);
   if (!senhaCorreta) return res.status(401).json({ msg: 'Senha incorreta' });
 
-  const token = jwt.sign({ id: usuario._id, isAdmin: usuario.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1d' });
+  const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
-  res.json({ token, usuario: { nome: usuario.nome, email: usuario.email, isAdmin: usuario.isAdmin } });
+  res.json({ token, user: { nome: user.nome, email: user.email, isAdmin: user.isAdmin } });
 };
