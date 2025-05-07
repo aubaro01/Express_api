9a46390a58e6ea71d357a7fabd4da8d134197c86
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-05-07 15:57:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config(); 
 const cors = require('cors');
-
+const path = require('path');
 const app = express(); 
 
 
@@ -10,7 +10,7 @@ app.use(cors({
 }));
 
 app.use(express.json()); 
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
